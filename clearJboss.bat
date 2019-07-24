528da9ecc9bc3527f94fa4948e8a6598fce58679
arconom <arconom@hotmail.com>
2019-07-24 11:00:42 -0700

    Delete clearJboss.bat

--- clearJboss.bat
+++
@@ -1,8 +0,0 @@
-call:clearDir "C:\DevEnv\jboss-eap-6.4hibernate\standalone\deployments"
-call:clearDir "C:\DevEnv\jboss-eap-6.4hibernate\standalone\log"
-call:clearDir "C:\DevEnv\jboss-eap-6.4hibernate\standalone\tmp"
-
-:clearDir
-cd /d %~1
-for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
-EXIT /B 0
