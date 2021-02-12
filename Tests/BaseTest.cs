fd4c346accd747b81d73c3d727ce065c570993e9
arconom <arconom@hotmail.com>
2021-02-12 09:53:26 -0800

    Update BaseTest.cs

--- Tests/BaseTest.cs
+++ Tests/BaseTest.cs
@@ -1,47 +1,40 @@
-import org.junit.After;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
-import org.openqa.selenium.WebDriver;
-import Steps.LoginSteps;
-import Steps.LogoutSteps;
-import common.Constants;
-import common.WebDriverFactory;
-
 /*
- * For information about the Screenplay pattern,
- * http://thucydides.info/docs/serenity-staging/#_serenity_and_the_screenplay_pattern
- *
- * and some good practices
- * https://www.blazemeter.com/blog/top-15-ui-test-automation-best-practices-you-should-follow
- */
-
-public abstract class BaseTest {
-	@AfterClass
-	public static void cleanUpAfterClass() {
-	}
-
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	protected WebDriver driver;
-
-	LoginSteps loginSteps = new LoginSteps();
-	LogoutSteps logoutSteps = new LogoutSteps();
-
-	@Before
-	public void setUp() throws Exception {
-		this.driver = WebDriverFactory
-				// .getChromeWebDriver(false);
-				.getIEWebDriver();
-		this.loginSteps.login_as(this.driver, Constants.admin);
-	}
-
-	@After
-	public void tearDown() throws Exception {
-		this.logoutSteps.logout(this.driver);
-		this.driver.close();
-	}
-
+* For information about the Screenplay pattern,
+* http://thucydides.info/docs/serenity-staging/#_serenity_and_the_screenplay_pattern
+*
+* and some good practices
+* https://www.blazemeter.com/blog/top-15-ui-test-automation-best-practices-you-should-follow
+*/
+using OpenQA.Selenium;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+namespace Selenium.Tests
+{
+    [TestClass]
+    public abstract class BaseTest
+    {
+        [ClassCleanup]
+        public static void ClassCleanup()
+        {
+        }
+        [ClassInitialize]
+        public static void ClassInitialize(TestContext context)
+        {
+        }
+        protected IWebDriver driver;
+        [TestInitialize]
+        //@Before
+        public virtual void Initialize()
+        {
+            this.driver = WebDriverFactory
+                     .getWebDriver(false);
+        }
+        [TestCleanup]
+        //@After
+        public virtual void Cleanup()
+        {
+            this.driver?.Close();
+            this.driver?.Quit();
+            this.driver?.Dispose();
+        }
+    }
 }
